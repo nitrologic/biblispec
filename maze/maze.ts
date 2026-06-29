19f127a4382c22989c18a3bcedc0117c70ed38ad
simon ryzen5 <nitrologic@gmail.com>
2026-06-30 10:31:46 +1200

    maze 0.5

--- maze/maze.ts
+++ maze/maze.ts
@@ -1,4 +1,4 @@
-console.log("maze 0.2");
+console.log("maze 0.5");
 
 const plain=[
 	"#################################",
@@ -14,6 +14,89 @@ const boxRounded="╭╮╰╯─┬┴│┤├┼";
 const boxDouble="╔╗╚╝═╦╩║╣╠╬";
 const boxSingle="┏┓┗┛━┳┻┃┫┣╋";
 
+enum Edge { CornerTopLeft, CornerTopRight, CornerBotLeft, CornerBotRight, LineHoriz, TeeDown, TeeUp, LineVert, TeeLeft, TeeRight, Cross }
+
+// if 3x3 has single solid corner then use Corner 
+
+function boxIndex(b9:[]):number{
+	if(b9=="▢▢▢▢▢▢▢▢▣") return Edge.CornerTopLeft;
+	if(b9=="▣▢▢▢▢▢▢▢▢") return Edge.CornerBotRight;
+	if(b9=="▢▢▣▢▢▢▢▢▢") return Edge.CornerBotLeft;
+	if(b9=="▢▢▢▢▢▢▣▢▢") return Edge.CornerTopRight;
+	if(b9=="▣▣▣▢▢▢▢▢▢") return Edge.LineHoriz;
+	if(b9=="▢▢▢▢▢▢▣▣▣") return Edge.LineHoriz;
+	if(b9=="▣▢▢▣▢▢▣▢▢") return Edge.LineVert;
+	if(b9=="▢▢▣▢▢▣▢▢▣") return Edge.LineVert;
+	console.log(b9);
+	return -1;
+}
+
+
+
+function getOutline(u: boolean, d: boolean, l: boolean, r: boolean): Edge {
+	if (u && d && l && r) return Edge.Cross;
+	if (u && d && l) return Edge.TeeLeft;
+	if (u && d && r) return Edge.TeeRight;
+	if (u && l && r) return Edge.TeeUp;
+	if (d && l && r) return Edge.TeeDown;
+	if (l && r) return Edge.LineHoriz;
+	if (u && d) return Edge.LineVert;
+	if (d) return r ? Edge.CornerTopLeft : Edge.CornerTopRight;
+	return r ?Edge.CornerBotLeft : Edge.CornerBotRight;
+}
+
+function getEdge(u: boolean, d: boolean, l: boolean, r: boolean): Edge {
+	if (u && d && l && r) return Edge.Cross;
+	if (u && d && l) return Edge.TeeLeft;
+	if (u && d && r) return Edge.TeeRight;
+	if (u && l && r) return Edge.TeeUp;
+	if (d && l && r) return Edge.TeeDown;
+	if (l && r) return Edge.LineHoriz;
+	if (u && d) return Edge.LineVert;
+	if (d) return r ? Edge.CornerTopLeft : Edge.CornerTopRight;
+	return r ?Edge.CornerBotLeft : Edge.CornerBotRight;
+}
+
+function udlr(a9):number{
+	let u = a9[1]===9635;
+	let d = a9[7]===9635;
+	let l = a9[3]===9635;
+	let r = a9[5]===9635;
+	if (u || d || l || r) {
+		let index=getEdge(u, d, l, r);
+		return index;
+	} else {
+		return -1;
+	}
+}
+
+function outlineGrid(lines:string[],boxChars:string){
+	let w=lines[0].length;
+	let h=lines.length;
+	let result=[];
+	for(let y=0;y<h;y++){
+		let line="";
+		for(let x=0;x<w;x++){
+			let char=lines[y].charAt(x);
+//			let a9:number[]=clampSample33(lines,x,y);
+			let a9:number[]=wrapSample33(lines,x,y);
+			if(a9.length!=9){
+				console.log("Error with a9");
+			}
+			if(char=="▢"&&a9.includes(9635)){//&&a9.includes(9634)){
+//				let index=udlr(a9);
+				let b9=String.fromCodePoint(...a9);
+				let index=boxIndex(b9);
+				if(index>=0) char=boxChars.charAt(index);else char="*";
+			}
+			line+=char;
+		}
+		result.push(line);
+	}
+	return result;
+}
+
+
 function doubleLines(lines:string[],x:number,y:number){
 	let w=lines[0].length;
 	let h=lines.length;
@@ -28,7 +111,7 @@ function doubleLines(lines:string[],x:number,y:number){
 	return result;
 }
 
-function tripleLines(lines:string[],x:number,y:number){
+function tripleLines(lines:string[],x:number,y:number):string[]{
 	let w=lines[0].length;
 	let h=lines.length;
 	let result=[];
@@ -42,7 +125,7 @@ function tripleLines(lines:string[],x:number,y:number){
 	return result;
 }
 
-function clampSample33(lines:string[],x:number,y:number){
+function clampSample33(lines:string[],x:number,y:number):number[]{
 	let w=lines[0].length;
 	let h=lines.length;
 	let result=[];
@@ -62,7 +145,7 @@ function clampSample33(lines:string[],x:number,y:number){
 	return result;
 }
 
-function wrapSample33(lines:string[],x:number,y:number){
+function wrapSample33(lines:string[],x:number,y:number):number[]{
 	let w=lines[0].length;
 	let h=lines.length;
 	try{
@@ -96,16 +179,17 @@ function blockHashGrid(lines:string[]){
 	for(let y=0;y<height;y++){
 		let line="";
 		for(let x=0;x<width;x++){
-			let block="▢ ";
+			let block="▢";
 			if(y>0 && y<height-1 && x>0 && x<width-1){
 				if(y>0 && y<height-1 && x>0 && x<width-1){
 					let col=((x-1)/2)|0;
 					let row=((y-1)/2)|0;
-					if(col<w && row <h){
-						let a9=wrapSample33(lines,col,row);
-//						let a9=clampSample33(lines,col,row);
-						if(a9.includes(35))	block="▣ ";
-//						if(!a9.includes(32))	block="▣ ";
+					if(col<w && row<h){
+//						let a9=wrapSample33(lines,col,row);
+						let a9=clampSample33(lines,col,row);
+						if(a9[4]==35) block="▣";
+//						if(a9.includes(35))	block="▣ ";
+//						if(!a9.includes(32)) block="▣ ";
 					}
 				}
 			}
@@ -119,12 +203,18 @@ function blockHashGrid(lines:string[]){
 let double=doubleLines(plain);
 let triple=tripleLines(plain);
 
-for(let line of triple){
+
+let src=double;
+
+
+for(let line of src){
 	console.log(line);
 }
 
-let grid=blockHashGrid(triple)
+let grid=blockHashGrid(src)
 
-for(let line of grid){
+let grid2=outlineGrid(grid,boxRounded);
+
+for(let line of grid2){
 	console.log(line);
 }
