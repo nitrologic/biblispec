8c52b9a3daeba6da5e58fdd20a94126d7f848f2c
simon ryzen5 <nitrologic@gmail.com>
2026-07-28 08:37:20 +1200

    bibli web 1

---
+++ bibli.js
@@ -0,0 +1,15 @@
+import spec from './biblispec.json' with { type: 'json' };
+//console.log("[bibli]",data); 
+//console.log("[bibli]",data.name);
+
+let terminalDiv;
+
+function append(text){
+	terminalDiv.textContent+=text;
+}
+function onLoad(){
+	terminalDiv=document.getElementById("terminal");
+	append("hello world");
+}
+
+window.onload=onLoad;
